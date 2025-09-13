21fdbe2be93d3fcca9308cbe79373cba8ba87df7
zachmshort <zach.short@fantomworks.com>
2025-09-13 19:58:13 -0400

    dummy data inside of input. some functionality with conditional toggling

--- frontend/components/features/friends/add-friends-input.tsx
+++ frontend/components/features/friends/add-friends-input.tsx
@@ -1,10 +1,21 @@
+'use client';
+
 import { Input } from '@/components/ui/input';
-import { SearchIcon } from 'lucide-react';
-import { useState } from 'react';
+import { SearchIcon, UserPlus } from 'lucide-react';
+import { Dispatch, SetStateAction, useState } from 'react';
 
 export function AddFriendsInput() {
   const [value, setValue] = useState('');
-  const [suggestions, setSuggestions] = useState([]);
+  const [suggestions, setSuggestions] = useState([
+    'test',
+    'test1',
+    'test2',
+    'test3',
+    'test3',
+    'test3',
+    'test3',
+    'test3',
+  ]);
   const [showSuggestions, setShowSuggestions] = useState(false);
 
   return (
@@ -20,17 +31,34 @@ export function AddFriendsInput() {
           }}
         />
         <SearchIcon className={`absolute top-2 right-3`} size={20} />
-        {showSuggestions && <Suggestions suggestions={suggestions} />}
+        {showSuggestions && (
+          <Suggestions setValue={setValue} suggestions={suggestions} />
+        )}
       </div>
     </>
   );
 }
 
-function Suggestions({ suggestions }: { suggestions: string[] }) {
+function Suggestions({
+  suggestions,
+  setValue,
+}: {
+  suggestions: string[];
+  setValue: Dispatch<SetStateAction<string>>;
+}) {
   return (
-    <div className={`h-60`}>
-      {suggestions.map((suggestion: string) => {
-        return <>{suggestion}</>;
+    <div className={`min-h-60 flex flex-col items-start mt-1`}>
+      {suggestions.map((suggestion: string, i: number) => {
+        return (
+          <button
+            key={i}
+            className={`flex flex-row justify-between items-center w-full p-1 px-4`}
+            onClick={() => setValue(suggestion)}
+          >
+            <p>{suggestion}</p>
+            <UserPlus size={10} />
+          </button>
+        );
       })}
     </div>
   );
